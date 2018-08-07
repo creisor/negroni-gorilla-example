b5098afa96d3928c2009cc5ac27ce91fb77c84bc
Chris Reisor <creisor@groupon.com>
2018-08-07 08:59:31 -0500

    this shows ability to mix routes hanging off slash with routes hanging off versioned path

--- main.go
+++ main.go
@@ -8,18 +8,18 @@ import (
 )
 
 func main() {
-	v1Router := mux.NewRouter().PathPrefix("/v1").Subrouter()
+	v1Router := mux.NewRouter()
 	v1 := v1.NewServer(v1Router, "Hello from v1\n")
 
-	v2Router := mux.NewRouter().PathPrefix("/v2").Subrouter()
+	v2Router := mux.NewRouter().PathPrefix("/v2/").Subrouter()
 	v2 := v2.NewServer(v2Router, "Hello from v2\n")
 
 	v1.Routes()
 	v2.Routes()
 
 	router := mux.NewRouter()
-	router.PathPrefix("/v1").Handler(v1Router)
 	router.PathPrefix("/v2").Handler(v2Router)
+	router.PathPrefix("/").Handler(v1Router)
 
 	n := negroni.Classic()
 	n.UseHandler(router)
